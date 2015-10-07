b1dd0293b444b7c34a3ba6d3bd82589e1379d740
Amohdoh <gabe@amohdoh.com>
2015-10-07 10:01:04 -0400

    0.05
    Added buttons

--- main.py
+++ main.py
@@ -16,24 +16,43 @@ from __future__ import print_function
 import pygame
 pygame.init()
 
-#Import
-startBack = pygame.image.load("img/StartScreenBackground.jpg")
 
 
 #Vars
 infoObject = pygame.display.Info()
 screenWidth, screenHeight = infoObject.current_w, infoObject.current_h
 menuB = 0
+#mpos = pygame.mouse.get_pos()
+###Import####
+#Backgrounds
+startBack = pygame.image.load("img/StartScreenBackground.jpg")
+startBack = pygame.transform.scale(startBack, (screenWidth, screenHeight))
+#Main Screen Buttons
+creditsBut = pygame.image.load("img/Credits_Button.png")
+creditsBut = pygame.transform.scale(creditsBut, (600, 400))
+creditsButOH = pygame.image.load("img/Credits_Button_On_Hover.png")
+creditsButOH = pygame.transform.scale(creditsButOH, (600, 400))
+settingsBut = pygame.image.load("img/Settings_Button.png")
+settingsBut = pygame.transform.scale(settingsBut, (600, 400))
+settingsButOH = pygame.image.load("img/Settings_Button_On_Hover.png")
+settingsButOH = pygame.transform.scale(settingsButOH, (600, 400))
+startBut = pygame.image.load("img/Start_Button.png")
+startBut = pygame.transform.scale(startBut, (600, 400))
+startButOH = pygame.image.load("img/Start_Button_On_Hover.png")
+startButOH = pygame.transform.scale(startButOH, (600, 400))
 
 # init
 screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.FULLSCREEN, 32)
 background = pygame.Surface(screen.get_size())
 screen.blit(startBack,(0,0))
+currentCred = creditsBut
+currentSet = settingsBut
+currentStr = startBut
 mainMenu = True
 
 # Pygame clock 
 clock = pygame.time.Clock()
- 
+
 
 # Desired framerate in frames per second. Try out other values.
 FPS = 30
@@ -46,7 +65,10 @@ while mainMenu:
     # Do not go faster than this framerate.
     milliseconds = clock.tick(FPS)
     playtime += milliseconds / 1000.0
- 
+    screen.blit(currentCred,(screenWidth/2 -300,screenHeight-400))
+    screen.blit(currentSet,(screenWidth/2 -300,screenHeight-600))
+    screen.blit(currentStr,(screenWidth/2 -300,screenHeight-800))
+    #while 
     for event in pygame.event.get():
         # User presses QUIT-button.
         if event.type == pygame.QUIT:
